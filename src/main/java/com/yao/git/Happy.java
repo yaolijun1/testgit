0d89632e50df0aceb5b40ddbe885e00a70b9e454
yao_glb <666_glb@yao.com>
2018-10-22 09:30:56 +0800

    edit on branch host_fix

--- src/main/java/com/yao/git/Happy.java
+++ src/main/java/com/yao/git/Happy.java
@@ -4,5 +4,6 @@ public class Happy {
 	public static void main(String[] args) {
 		System.out.println("Left  TestGit....");
 		System.out.println("Right TestGit....");
+		System.out.println("hot TestGit....");
 	}
 }
