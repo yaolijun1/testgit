ca2c664e257ffde1f18f2c909144970ec3dfa4f9
yao_glb <666_glb@yao.com>
2018-10-23 18:52:50 +0800

    tao

--- src/main/java/com/yao/git/Happy.java
+++ src/main/java/com/yao/git/Happy.java
@@ -3,7 +3,7 @@ package com.yao.git;
 public class Happy {
 	public static void main(String[] args) {
 		System.out.println("Left  TestGit....");
-		System.out.println("Right TestGit....");
+		System.out.println("Right2 TestGit....");
 		System.out.println("hot TestGit....");
 		System.out.println("hot TestGit....");
 	}
